c6d235a0d02145a3767fc369e2e6326d428f3d1e
Victor Assis <victor_assis@poli.ufrj.br>
2023-06-10 11:10:36 -0300

    First queries and Env variables with Zod

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,31 @@
 import fastify from "fastify";
+import crypto from "node:crypto";
 import { knex } from "./database";
+import { env } from "./env";
 
 const app = fastify();
 
 app.get("/", async () => {
-  const transaction = await knex("transactions").insert({});
+  const transaction = await knex("transactions")
+    .insert({
+      id: crypto.randomUUID(),
+      title: "Transação de teste",
+      amount: 1000,
+    })
+    .returning("*");
+
+  return transaction;
+});
+
+app.get("/get", async () => {
+  const transaction = await knex("transactions").select("*");
+
+  return transaction;
 });
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log("HTTP Server Running at http://localhost:3333!");
