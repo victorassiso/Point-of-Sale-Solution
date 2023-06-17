b7d085394398f9737f6c16da1a85a9084e21c738
luiz.carrion <luiz.carrion@ip.tv>
2023-06-17 10:25:16 -0300

    recreates the frontend structure

---
+++ frontend/react-admin/src/scenes/global/Topbar.jsx
@@ -0,0 +1,20 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material";
+
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutilinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+
+const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return <div>Topbar</div>;
+};
+
+export default Topbar;
