2d8eda4406b488f766f56d123f108a5898e5ae91
luiz.carrion <luiz.carrion@ip.tv>
2023-06-17 12:07:53 -0300

    create products table

---
+++ frontend/react-admin/src/data/mockData.js
@@ -0,0 +1,16 @@
+import { tokens } from "../theme";
+
+export const mockDataTeam = [
+  {
+    id: 1,
+    name: "Produto 1",
+    price: 40.0,
+    status: true,
+  },
+  {
+    id: 2,
+    name: "Produto 2",
+    price: 100.0,
+    status: false,
+  },
+];
--- frontend/react-admin/src/scenes/products/index.jsx
+++ frontend/react-admin/src/scenes/products/index.jsx
@@ -1,5 +1,45 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+
+import Header from "../../components/Header";
+import { mockDataTeam } from "../../data/mockData";
+
 const Products = () => {
-  return <div>Products</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  const columns = [
+    { field: "id", headerName: "ID" },
+    {
+      field: "name",
+      headerName: "Nome",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "price",
+      headerName: "Preço",
+      type: "number",
+      headerAlign: "left",
+      align: "left",
+    },
+    {
+      field: "status",
+      headerName: "Status",
+      type: "boolean",
+    },
+  ];
+
+  return (
+    <Box m="20px">
+      <Header title={"Produtos"} subtitle={"Listagem dos produtos"} />
+
+      <Box m="40px 0 0 0" height="75vh">
+        <DataGrid rows={mockDataTeam} columns={columns} />
+      </Box>
+    </Box>
+  );
 };
 
 export default Products;
