77ea2e9c5a905277d99adab327439af3f96381c8
Victor Assis <victor_assis@poli.ufrj.br>
2023-06-18 19:39:45 -0300

    Fix: Delete old frontend versions

--- frontendv2/src/data/mockProductsData.js
+++
@@ -1,1 +0,0 @@
-export const status = ["active", "inactive"];
--- frontendv2/src/use-cases/factories/make-create-product-use-case.ts
+++
@@ -1,9 +0,0 @@
-import { AxiosProductsRepository } from "../../repositories/axios/axios-products-repository";
-import { CreateProductUseCase } from "../create-product-use-case";
-
-export function makeCreateProductUseCase() {
-  const productsRepository = new AxiosProductsRepository();
-  const createProductUseCase = new CreateProductUseCase(productsRepository);
-
-  return createProductUseCase;
-}
